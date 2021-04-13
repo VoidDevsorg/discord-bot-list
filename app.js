6a3dc152cb14a555c78b0e5050a9a8b6092dc06b
Claudette <76551996+iClaudette@users.noreply.github.com>
2021-04-13 16:05:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -539,7 +539,7 @@ app.get("/error", (req, res) => {
     });
     app.post("/admin/decline/:botID", checkMaintence, checkAdmin, checkAuth, async (req, res) => {
         let rBody = req.body;
-	 let botadata = await botsdata.findOne({ botID: req.params.botID });
+	 let botdata = await botsdata.findOne({ botID: req.params.botID });
          client.users.fetch(botdata.ownerID).then(sahip => {
              client.channels.cache.get(channels.botlog).send(`<@${botdata.ownerID}>'s bot named **${botdata.username}** has been declined. `)
              client.users.cache.get(botdata.ownerID).send(`Your bot named **${botdata.username}** has been declined.\nReason: **${rBody['reason']}**\nAuthorized: **${req.user.username}**`)
