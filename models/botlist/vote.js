ffe45229cdc7249bf424e005f01ba5dcdb6cb1f1
Claudette <76551996+iClaudette@users.noreply.github.com>
2021-04-19 00:12:29 +0300

    Delete models directory

--- models/botlist/vote.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const schema = new mongoose.Schema({
-user: String,
-bot: String,
-ms: Number,
-Date: Date
-})
-module.exports = mongoose.model('votes', schema)
