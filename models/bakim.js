5cd2f9f9d4ad9a9e4d99493fbf772acf218fda97
Claudette <76551996+iClaudette@users.noreply.github.com>
2021-05-02 18:14:05 +0300

    Delete models directory

--- models/bakim.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-let hm = new mongoose.Schema({
-server: String,
-reason: String,
-bakimmsg: String
-});
-
-module.exports = mongoose.model("bakim", hm);
--- models/lastupdate.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-let hm = new mongoose.Schema({
-author: String,
-authorID: String,
-updateMessage: String,
-});
-
-module.exports = mongoose.model("lastupdates", hm);
