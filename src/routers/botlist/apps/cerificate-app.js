1f1c57de88a7d7cc2fa3cbcff1741bc26bd1e17c
iClaudette <76551996+iClaudette@users.noreply.github.com>
2021-07-18 22:16:06 +0300

    Fixed: Certificate App

--- src/routers/botlist/apps/cerificate-app.js
+++ src/routers/botlist/apps/cerificate-app.js
@@ -1,7 +1,7 @@
 const app = require('express').Router();
 const botsdata = require("../../../database/models/botlist/bots.js");
 const apps = require("../../../database/models/botlist/certificate-apps.js");
-
+const client = global.Client;
 console.log("[vcodes.xyz]: Botlist/Certificate Application router loaded.");
 
 app.get("/certification/apply", global.checkAuth, async (req, res) => {
@@ -25,11 +25,11 @@ app.post("/certification/apply", global.checkAuth, async (req, res) => {
         botID: rBody['bot']
     })
     if(!botdata) return res.redirect('/error?code=404&message=You entered invalid bot id.');
-    if (req.user.id != botdata.ownerID || !botdata.coowners.includes(req.user.id)) return res.redirect('/error?code=404&message=You entered invalid bot id.');
+    // if(req.user.id !== botdata.ownerID || !botdata.coowners.includes(req.user.id)) return res.redirect('/error?code=404&message=You entered invalid bot id.');
     if(botdata.certificate === "Certified") return res.redirect('/error?code=401&message=This bot already certified.');
-    let findBot = await appsdata.findOne({ botID: rBody['bot'] });
+    let findBot = await apps.findOne({ botID: rBody['bot'] });
     if(findBot) return res.redirect('/error?code=401&message=This bot application already applied.')
-    new appsdata({
+    new apps({
         botID: rBody['bot'],
         future: rBody['future']
     }).save();
