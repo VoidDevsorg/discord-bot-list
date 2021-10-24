6f9b4d06cdae5dc999e5d3216e661d73c160171a
FusionsDev <89275160+FusionsDev@users.noreply.github.com>
2021-10-24 02:00:24 +0100

    Fixed Add.JS Error
    Missed one ' ! Fixed it

--- src/routers/admin/codeshare/add.js
+++ src/routers/admin/codeshare/add.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js);
+const Discord = require('discord.js')
 const app = require('express').Router();
 const codesSchema = require("../../../database/models/codes.js");
 const client = global.Client;
