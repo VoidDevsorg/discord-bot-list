5f2e644213d4ab42327aaebaabf2209c60ae090b
EliteNover <88671792+EliteNover@users.noreply.github.com>
2022-01-25 19:03:07 +0000

    Update botinfo.js

--- src/commands/botinfo.js
+++ src/commands/botinfo.js
@@ -17,7 +17,7 @@ module.exports.run = async (client,message,args) => {
    const embed = new Discord.MessageEmbed()
    .setThumbnail(b.avatar)
    .setAuthor(b.username+"#"+b.discrim, b.avatar)
-   .setDescription("**[Vote for "+b.username+"#"+b.discrim+" on the vCodes website.](https://vcodes.xyz/bot/"+b.botID+"/vote)**")
+   .setDescription("**[Vote for "+b.username+"#"+b.discrim+" on the vCodes website](https://vcodes.xyz/bot/"+b.botID+"/vote)**")
    .addField("ID", b.botID, true)
    .addField("Username", b.username, true)
    .addField("Discriminator", b.discrim, true)
