8caaf39d721b7f92f64557bb93badc3ce7ee3e2a
ByQush <mehmettaskn@yahoo.com>
2022-02-16 22:31:24 +0300

    void.db has been deprecated so we switched quick.db

--- src/server.js
+++ src/server.js
@@ -1194,36 +1194,35 @@ module.exports = async (client) => {
         })
         return res.redirect('../admin/userban?success=true&message=User ban removed.');
     });
-
-    app.get("/admin/partners", checkMaintence, checkAdmin, checkAuth, async (req, res) => {
-        if (!config.bot.owners.includes(req.user.id)) return res.redirect('../admin');
-        const Database = require("void.db");
-        const db = new Database(path.join(__dirname, './database/json/partners.json'));
-        renderTemplate(res, req, "/admin/administrator/partners.ejs", {
-            req,
+    
+    
+    app.get("/admin/partners", global.checkAuth, async (req, res) => {
+    if (!config.bot.owners.includes(req.user.id)) return res.redirect('../admin');
+    const Database = require("quick.db");
+	renderTemplate(res, req, "/admin/administrator/partners.ejs", {
+	        req,
             roles,
             config,
             db
-        })
-    });
-    app.post("/admin/partners", checkMaintence, checkAdmin, checkAuth, async (req, res) => {
-        if (!config.bot.owners.includes(req.user.id)) return res.redirect('../admin');
-        const Database = require("void.db");
-        const db = new Database(path.join(__dirname, './database/json/partners.json'));
-        db.push(`partners`, {
-            code: createID(12),
-            icon: req.body.icon,
-            ownerID: req.body.ownerID,
-            serverName: req.body.serverName,
-            website: req.body.Website,
-            description: req.body.partnerDesc
-        })
-        let x = client.guilds.cache.get(config.server.id).members.cache.get(req.body.ownerID)
-        if (x) {
-            x.roles.add(roles.profile.partnerRole)
-        }
-        return res.redirect('/admin/partners?success=true&message=Partner added.')
-    });
+	 })
+});
+app.post("/admin/partners", global.checkAuth, async (req, res) => {
+    if (!config.bot.owners.includes(req.user.id)) return res.redirect('../admin');
+    const Database = require("quick.db");
+    Database.push(`partners`, {
+        code: createID(12),
+        icon: req.body.icon,
+        ownerID: req.body.ownerID,
+        serverName: req.body.serverName,
+        website: req.body.Website,
+        description: req.body.partnerDesc
+    })
+    let x = client.guilds.cache.get(config.server.id).members.cache.get(req.body.ownerID)
+    if (x) {
+        x.roles.add(roles.profile.partnerRole)
+    }
+    return res.redirect('/admin/partners?success=true&message=Partner added.')
+});
     //---------------- ADMIN ---------------\\
 
     //------------------- PROFILE -------------------//
