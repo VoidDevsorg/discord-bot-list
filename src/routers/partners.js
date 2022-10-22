c40d4813d6b6d5014afd536e5d1eeb60d5a69bfd
JanjyTapYT <janjytapyt@sdevs.org>
2022-10-22 11:52:30 +0200

    Update partners.js

--- src/routers/partners.js
+++ src/routers/partners.js
@@ -3,7 +3,7 @@ const Database = require("quick.db");
 const path = require("path")
 const db = new Database(path.join(__dirname, '../database/json/partners.json'));
 
-console.log("[stoneclane.xyz]: Partners router loaded.");
+console.log("[vcodes.xyz]: Partners router loaded.");
 
 app.get("/partners", async (req,res) => {
     res.render("partners.ejs", {
