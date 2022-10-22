d721aa11fd54a03942ff65a36d54c20c1b38f692
JanjyTapYT <janjytapyt@sdevs.org>
2022-10-22 11:48:09 +0200

    Update partners.js

--- src/routers/partners.js
+++ src/routers/partners.js
@@ -1,12 +1,13 @@
 const app = require('express').Router();
-const Database = require("quick.db");
+const Database = require("void.db");
 const path = require("path")
+const db = new Database(path.join(__dirname, '../database/json/partners.json'));
 
-console.log("[vcodes.xyz]: Partners router loaded.");
+console.log("[stoneclane.xyz]: Partners router loaded.");
 
 app.get("/partners", async (req,res) => {
     res.render("partners.ejs", {
-    	bot: global.Client,
+        bot: global.Client,
         path: req.path,
         config: global.config,
         user: req.isAuthenticated() ? req.user : null,
