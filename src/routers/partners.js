29a423a1cc33cd7a192283476512e1fbbd13ffc0
DevMirza <pzhafeez@gmail.com>
2023-02-09 18:08:16 +0000

    formatting started

--- src/routers/partners.js
+++ src/routers/partners.js
@@ -1,19 +1,19 @@
-const app = require('express').Router();
+const app = require("express").Router();
 const Database = require("quick.db");
-const path = require("path")
+const path = require("path");
 
 console.log("[vcodes.xyz]: Partners router loaded.");
 
-app.get("/partners", async (req,res) => {
-    res.render("partners.ejs", {
-    	bot: global.Client,
-        path: req.path,
-        config: global.config,
-        user: req.isAuthenticated() ? req.user : null,
-        req: req,
-        db: Database,
-        roles: global.config.server.roles
-    })
-})
+app.get("/partners", async (req, res) => {
+  res.render("partners.ejs", {
+    bot: global.Client,
+    path: req.path,
+    config: global.config,
+    user: req.isAuthenticated() ? req.user : null,
+    req: req,
+    db: Database,
+    roles: global.config.server.roles,
+  });
+});
 
 module.exports = app;
